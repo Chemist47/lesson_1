fec23d78f2d6efc8f28fc9f057afcb4da24739fd
unknown <vcslvalesin@gmail.com>
2023-11-05 16:53:33 +0400

    add css

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const routing = require('./routing');
+
+
+let server = new http.Server(function(req, res) {
+  var jsonString = '';
+  res.setHeader('Content-Type', 'application/json');
+  req.on('data', (data) => { // Пришла информация - записали.
+      jsonString += data;
+  });
+
+  req.on('end', () => {// Информации больше нет - передаём её дальше.
+      routing.define(req, res, jsonString); // Функцию define мы ещё не создали.
+  });
+});
+server.listen(8000, 'localhost');
